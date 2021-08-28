daf467ba4395f8621e98df5a9568a54e2d882db8
Fernando B Calo <Fernandob.calo@outlook.com>
2021-08-28 17:13:42 -0300

    aula 6 P1

---
+++ src/app/photos/photo-list/photo-list-module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { PhotoComponent } from "../photo/photo.component";
+import { PhotoModule } from "../photo/photo.module";
+import { filterByDescription } from "./filter-by-description.pipe";
+import { LoadButtonComponent } from "./load-button/load-button.component";
+import { PhotoListComponent } from "./photo-list.component";
+import { PhotosComponent } from "./photos/photos.component";
+import { CardModule } from "./shared/components/card/card.module";
+import { DarkenOnHoverModule } from "./shared/components/directives/darken-on-hover/darken-on-hover.module";
+
+@NgModule({
+    declarations: [
+        PhotoListComponent,
+        PhotosComponent,
+        LoadButtonComponent,
+        filterByDescription,
+        //SearchComponent
+    ],
+    imports: [ 
+        CommonModule,
+        PhotoModule,
+        CardModule, 
+        DarkenOnHoverModule
+    ]
+})
+
+export class PhotoListModule{}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,24 +3,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { filterByDescription } from './photo-list/filter-By-Description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list-module';
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent, 
-        PhotoListComponent, 
-        PhotoFormComponent, 
-        PhotosComponent ,
-        filterByDescription,
-        LoadButtonComponent
-    ],
     imports: [ 
-        HttpClientModule,
-        CommonModule 
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
+        //DarkenOnHoverModule
     ]
 })
 export class PhotosModule {}
