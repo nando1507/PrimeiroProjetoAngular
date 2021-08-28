8eb4ccef03057fba64d8a24bfa11975b87e4743e
Fernando B Calo <Fernandob.calo@outlook.com>
2021-08-28 16:14:29 -0300

    aula 5

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,31 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
 
+import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
-
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit , OnDestroy {
 
-  photos: any[] = []; 
+  photos: Photo[] = []; 
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
   
   constructor(
     private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) { }
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 
   ngOnInit(): void {
-    
-      const userName = this.activatedRoute
-        .snapshot
-        .params
-        .userName;
-
-      this.photoService
-        .listFromUser(userName)
-        .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data['photos'];  
+    this.userName = this.activatedRoute.snapshot.data['photos'];
+    // this.debounce.next('f');
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if(!photos.length){
+          this.hasMore = false;
+        }
+
+      });
+  }
+
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,12 +5,18 @@ import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+import { filterByDescription } from './photo-list/filter-By-Description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
     declarations: [ 
         PhotoComponent, 
         PhotoListComponent, 
-        PhotoFormComponent 
+        PhotoFormComponent, 
+        PhotosComponent ,
+        filterByDescription,
+        LoadButtonComponent
     ],
     imports: [ 
         HttpClientModule,
